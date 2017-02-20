2efd01496c6335cd6b2e07b1df397c48c463c45d
Jussi Nurminen <jnu@iki.fi>
2017-02-20 15:29:40 +0200

    fix plot move

--- nexus_scripts/nexus_kinallplot.py
+++ nexus_scripts/nexus_kinallplot.py
@@ -17,11 +17,15 @@ def do_plot():
     pl.layout = layouts.std_kinall
     maintitleprefix = 'Kinetics/kinematics plot for '
 
+
     for vidfile in pl.trial.video_files:
         pl.external_play_video(vidfile)
 
-    pl.plot_trial(maintitleprefix=maintitleprefix)
+    pl.plot_trial(maintitleprefix=maintitleprefix, show=False)
     pl.move_plot_window(10, 30)
+    pl.show()
+
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)

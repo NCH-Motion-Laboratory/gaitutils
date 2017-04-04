cd2175a349c0a8bc3cfe18eed46095f0dbf6aed8
Jussi Nurminen <jnu@iki.fi>
2017-04-04 15:42:45 +0300

    exceptions

---
+++ gaitutils/exceptions.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+exceptions.py - custom exceptions
+
+@author: hus20664877
+"""
+
+
+class GaitDataError(Exception):
+    """ Exception specific to erroneous or unexpected gait data """
+    pass

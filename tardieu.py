d0affb561e291de787004d3518f5aa6d693fcf0a
Jussi Nurminen <jnu@iki.fi>
2017-04-25 15:35:27 +0300

    font fixes

--- tardieu.py
+++ tardieu.py
@@ -17,6 +17,14 @@ import numpy as np
 import btk
 
 
+def _adj_fonts(ax):
+    ax.xaxis.label.set_fontsize(cfg.plot.label_fontsize)
+    ax.yaxis.label.set_fontsize(cfg.plot.label_fontsize)
+    ax.title.set_fontsize(cfg.plot.title_fontsize)
+    ax.tick_params(axis='both', which='major',
+                   labelsize=cfg.plot.ticks_fontsize)
+
+
 vicon = nexus.viconnexus()
 
 # plot EMG vs. frames
@@ -24,7 +32,7 @@ pl = Plotter()
 pl.layout = [['L_TibAnt'], [None], [None]]
 pl.open_nexus_trial()
 pl.plot_trial(model_cycles=None, emg_cycles=None, x_axis_is_time=False,
-              plot_emg_rms=True)
+              plot_emg_rms=True, emg_tracecolor='b')
 
 # get marker data
 fs = pl.trial.framerate
@@ -36,23 +44,24 @@ Pknee = data['Knee_P']
 Pall = np.stack([Ptoe, Pank, Pknee], axis=1)
 # compute segment angles (deg)
 ang = segment_angles(Pall)
+# normalize according to initial pos
+ang -= ang[~np.isnan(ang)][0]
+# plot angular velocity -> deg/s
+angd = fs * np.diff(ang, axis=0)
 
-# angle
+# plot angle
 ax = plt.subplot(pl.gridspec[1], sharex=pl.axes[0])
 ax.plot(pl.trial.t+pl.trial.offset, ang / np.pi * 180)
-ax.set(ylabel='Angle (deg)')
-ax.xaxis.label.set_fontsize(cfg.plot.label_fontsize)
-ax.yaxis.label.set_fontsize(cfg.plot.label_fontsize)
-ax.tick_params(axis='both', which='major', labelsize=cfg.plot.ticks_fontsize)
+ax.set(ylabel='deg')
+ax.set_title('Angle')
+_adj_fonts(ax)
 
-# angular velocity -> deg/s
-angd = fs * np.diff(ang, axis=0)
+# plot angular velocity
 ax = plt.subplot(pl.gridspec[2], sharex=pl.axes[0])
 ax.plot(pl.trial.t[:-1]+pl.trial.offset, angd / np.pi * 180)
-ax.set(xlabel='Frame', ylabel='Angular velocity (deg/s)')
-ax.xaxis.label.set_fontsize(cfg.plot.label_fontsize)
-ax.yaxis.label.set_fontsize(cfg.plot.label_fontsize)
-ax.tick_params(axis='both', which='major', labelsize=cfg.plot.ticks_fontsize)
+ax.set(xlabel='Frame', ylabel='deg/s')
+ax.set_title('Angular velocity')
+_adj_fonts(ax)
 
 # read events
 evs = vicon.GetEvents('4-511', 'General', 'MuscleOn')[0]

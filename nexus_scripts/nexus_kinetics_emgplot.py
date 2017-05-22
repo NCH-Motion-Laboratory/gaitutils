8cb780f435050e0acbb76372138ec5ac86f58eba
Jussi Nurminen <jnu@iki.fi>
2017-05-22 14:42:13 +0300

    force side for kinetics_emg

--- nexus_scripts/nexus_kinetics_emgplot.py
+++ nexus_scripts/nexus_kinetics_emgplot.py
@@ -9,19 +9,27 @@ Kinetics-EMG plot from Nexus.
 
 from gaitutils import Plotter, register_gui_exception_handler, cfg
 import logging
+import argparse
 
 
-def do_plot():
+def do_plot(force_side=None):
+    """ Side will be autodetected unless force_side is set """
 
     pl = Plotter()
     pl.open_nexus_trial()
-    sides = pl.trial.fp_events['valid']
-    if not sides:
-        raise Exception('No kinetics available')
-    elif sides == 'LR':
-        sides = ['L', 'R']
+
+    sides = pl.trial.fp_events['valid'] if force_side == None else force_side
+          
+    if force_side is None:  # autodetect
+        if not sides:
+            raise Exception('No kinetics available')
+        elif sides == 'LR':
+            sides = ['L', 'R']
+        else:
+            sides = [sides]
     else:
-        sides = [sides]
+        sides = [side.upper() for side in force_side]
+
     for side in sides:
         pl.layout = (cfg.layouts.lb_kinetics_emg_r if side == 'R' else
                      cfg.layouts.lb_kinetics_emg_l)
@@ -30,11 +38,22 @@ def do_plot():
         maintitle = 'Kinetics-EMG (%s) for %s' % (s,
                                                   pl.title_with_eclipse_info())
         # for EMG, plot only the cycle that has kinetics info
-        pl.plot_trial(maintitle=maintitle)
+        pl.plot_trial(maintitle=maintitle, match_pig_kinetics=
+                      False if force_side else True)
         pdf_name = 'Kinetics_EMG_%s_%s.pdf' % (pl.trial.trialname, s)
         pl.create_pdf(pdf_name=pdf_name)
 
 if __name__ == '__main__':
+    
     logging.basicConfig(level=logging.DEBUG)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--side', type=str, nargs='+',
+                        help='strings that must appear in trial name',
+                        choices=['R','L','r','l'])
+    args = parser.parse_args()
+    
     register_gui_exception_handler()
-    do_plot()
+    do_plot(force_side=args.side)
+
+

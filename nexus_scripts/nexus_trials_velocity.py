efcf0c666b1a3c835e75fb53490fa6c7ca53f59d
Jussi Nurminen <jnu@iki.fi>
2017-06-21 14:33:43 +0300

    check for errors

--- nexus_scripts/nexus_trials_velocity.py
+++ nexus_scripts/nexus_trials_velocity.py
@@ -5,28 +5,32 @@ Created on Wed Jun 21 13:48:27 2017
 @author: HUS20664877
 """
 
-from gaitutils import nexus, cfg, utils, read_data, eclipse
+from gaitutils import nexus, cfg, utils, read_data, eclipse, c3d
+from gaitutils.exceptions import GaitDataError
 import numpy as np
 import matplotlib.pyplot as plt
 import os.path as op
 
 
-def _trial_index(c3d):
+def _trial_index(c3dfile):
     try:
-        return int(op.splitext(c3d)[0][-3:])
+        return int(op.splitext(c3dfile)[0][-3:])
     except ValueError:
         try:
-            return int(op.splitext(c3d)[0][-2:])
+            return int(op.splitext(c3dfile)[0][-2:])
         except ValueError:
             return None
 
 
 def trial_median_velocity(source):
-    frate = read_data.get_metadata(source)['framerate']
-    dim = utils.principal_movement_direction(source, cfg.autoproc.
-                                             track_markers)
-    mkr = cfg.autoproc.track_markers[0]
-    vel_ = read_data.get_marker_data(source, mkr)[mkr+'_V'][:, dim]
+    try:
+        frate = read_data.get_metadata(source)['framerate']
+        dim = utils.principal_movement_direction(source, cfg.autoproc.
+                                                 track_markers)
+        mkr = cfg.autoproc.track_markers[0]
+        vel_ = read_data.get_marker_data(source, mkr)[mkr+'_V'][:, dim]
+    except (GaitDataError, ValueError):
+        return np.nan
     vel = np.median(np.abs(vel_[np.where(vel_)]))
     return vel * frate / 1000.
 
@@ -38,11 +42,10 @@ def do_plot():
              eclipse.get_eclipse_keys(enf,
                                       return_empty=True)['TYPE'] == 'Dynamic']
     c3ds = [nexus.enf2c3d(enf) for enf in enfs_]
-
-    indices = [_trial_index(c3d) for c3d in c3ds]
+    indices = [_trial_index(file) for file in c3ds]
     vels = np.array([trial_median_velocity(trial) for trial in c3ds])
 
-    vavg = vels.mean()
+    vavg = np.nanmean(vels)
 
     plt.stem(indices, vels)
     plt.ylabel('Velocity (m/s)')

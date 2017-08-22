a700df2ef230a7f4917ea2b7bbdb1cd32c439202
Jussi Nurminen <jnu@iki.fi>
2017-08-22 19:16:37 +0300

    don't run plots in a worker thread
    Conflicts:
    	nexus_scripts/nexus_menu.py

--- nexus_scripts/nexus_menu.py
+++ nexus_scripts/nexus_menu.py
@@ -78,21 +78,36 @@ class Gaitmenu(QtWidgets.QMainWindow):
         # load user interface made with designer
         uifile = resource_filename(__name__, 'nexus_menu.ui')
         uic.loadUi(uifile, self)
-        
-        self.btnEMG.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_emgplot.do_plot))
-        self.btnKinEMG.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_kinetics_emgplot.do_plot))
-        self.btnKinall.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_kinallplot.do_plot))
-        self.btnTardieu.clicked.connect(lambda ev: self._run_in_worker_thread(self._tardieu))
+        """ Stuff that shows matplotlib plots cannot be run in directly in
+        worker threads. To put plotting stuff into a worker thread, need to:
+        -make the plotting function return a figure (and not invoke the qt 
+        event loop)
+        -put the plotting function into a worker thread
+        -call plotting function
+        -create a qt dialog and canvas in GUI thread
+        -draw resulting figure onto canvas
+        So far this has not been done since the plotting functions return
+        rather quickly.
+        Running the plotting functions directly from the GUI thread is also a
+        bit ugly since the mpl event loop gets called twice, but this does not
+        seem to do any harm.
+        _execute runs the given function and handles exceptions. Passing
+        thread=True runs it in a separate worker thread, enabling GUI updates
+        (e.g. logging dialog) which can be nice.
+        """
+        self.btnEMG.clicked.connect(lambda ev: self._execute(nexus_emgplot.do_plot))
+        self.btnKinEMG.clicked.connect(lambda ev: self._execute(nexus_kinetics_emgplot.do_plot))
+        self.btnKinall.clicked.connect(lambda ev: self._execute(nexus_kinallplot.do_plot))
+        self.btnTardieu.clicked.connect(lambda ev: self._execute(self._tardieu))
         if have_custom:
-            self.btnCustom.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_customplot.do_plot))
+            self.btnCustom.clicked.connect(lambda ev: self._execute(nexus_customplot.do_plot))
         else:
             self.btnCustom.clicked.connect(self._no_custom)
-        self.btnTrialVelocity.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_trials_velocity.do_plot))
-        self.btnEMGCons.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_emg_consistency.do_plot))
-        self.btnKinCons.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_kin_consistency.do_plot))
-        self.btnAutoprocTrial.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_autoprocess_current.autoproc_single))
-        self.btnAutoprocSession.clicked.connect(lambda ev: self._run_in_worker_thread(nexus_autoprocess_trials.autoproc_session))
-        
+        self.btnTrialVelocity.clicked.connect(lambda ev: self._execute(nexus_trials_velocity.do_plot))
+        self.btnEMGCons.clicked.connect(lambda ev: self._execute(nexus_emg_consistency.do_plot))
+        self.btnKinCons.clicked.connect(lambda ev: self._execute(nexus_kin_consistency.do_plot))
+        self.btnAutoprocTrial.clicked.connect(lambda ev: self._execute(nexus_autoprocess_current.autoproc_single, thread=True))
+        self.btnAutoprocSession.clicked.connect(lambda ev: self._execute(nexus_autoprocess_trials.autoproc_session, thread=True))
         self.btnQuit.clicked.connect(self.close)
 
         # collect operation widgets
@@ -123,6 +138,7 @@ class Gaitmenu(QtWidgets.QMainWindow):
                             'nexus_scripts/nexus_customplot.py')
 
     def _exception(self, e):
+        logging.debug('Caught exception while running task')
         self.message_dialog('%s' % str(e))
 
     def _disable_op_buttons(self):
@@ -143,10 +159,23 @@ class Gaitmenu(QtWidgets.QMainWindow):
 
     def _finished(self):
         self._enable_op_buttons()
+        
+    def _execute(self, fun, thread=False):
+        """ Run some function. If thread==True, run in a separate worker
+        thread. """
+        self._disable_op_buttons()
+        if thread:
+            self._run_in_worker_thread(fun)
+        else:
+            try:
+                fun()
+            except Exception as e:
+                self._exception(e)
+            finally:
+                self._enable_op_buttons()
 
     def _run_in_worker_thread(self, fun):
         """ Run function in a separate thread """
-        self._disable_op_buttons()
         self.runner = Runner(fun)
         self.runner.signals.finished.connect(self._finished)
         self.runner.signals.error.connect(lambda e: self._exception(e))
@@ -171,7 +200,6 @@ class Runner(QRunnable):
         try:
             self.fun()
         except Exception as e:
-            logging.debug('Caught exception while running task')
             self.signals.error.emit(e)
         finally:
             self.signals.finished.emit()

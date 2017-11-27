0a1ffba0facfbf47aa1c6ea3197fa5de0af9118c
Jussi Nurminen <jnu@iki.fi>
2017-11-27 14:33:22 +0200

    fix hetu check

--- gaitutils/numutils.py
+++ gaitutils/numutils.py
@@ -14,13 +14,16 @@ from numpy.lib.stride_tricks import as_strided
 
 
 def check_hetu(hetu):
-    """ This checks Finnish social security numbers (hetu) """
+    """ This checks validity of a Finnish social security number (hetu) """
     chrs = "0123456789ABCDEFHJKLMNPRSTUVWXY"
-    if '-' in hetu:
-        hetu = hetu.replace('-', '')
-    if len(hetu) != 10:
+    if len(hetu) != 11:
         return False
-    chk = chrs[int(hetu[:-1]) % 31]
+    # check day and month
+    pp, kk = int(hetu[:2]), int(hetu[2:4])
+    if not (0 <= pp <= 31 and 1 <= kk <= 12):
+        return False
+    # check 'checksum'
+    chk = chrs[(int(hetu[:6] + hetu[7:10])) % 31]
     if hetu[-1] != chk:
         return False
     return True

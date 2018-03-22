7fb278f2fd9a9127794f4769bf59a282ee54b630
Jussi Nurminen <jnu@iki.fi>
2018-03-22 17:24:05 +0200

    tentatively ok

--- gaitutils/nexus_scripts/nexus_kin_consistency.py
+++ gaitutils/nexus_scripts/nexus_kin_consistency.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep 03 14:54:34 2015
 
 Kin* consistency plot from Nexus. Automatically picks trials based on Eclipse
 description and defined search strings.
@@ -19,13 +18,16 @@ from gaitutils.nexus import find_tagged
 logger = logging.getLogger(__name__)
 
 
-def do_comparison_plot(sessions, tags, show=True):
+def do_session_comparison_plot(sessions, tags, show=True):
+    """ Find trials according to tags in each session and superpose all
+    FIXME: this also works for one session?? (colors?) """
 
     pl = Plotter()
     pl.layout = cfg.layouts.overlay_lb_kin
     linecolors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'pink']
+    linecolors.reverse()  # FIXME
 
-    for session, linecolor in zip(sessions, linecolors):
+    for session in sessions:
         c3ds = find_tagged(tags=tags, sessionpath=session)
         if not c3ds:
             raise GaitDataError('No trials found for session %s' % session)
@@ -34,7 +36,7 @@ def do_comparison_plot(sessions, tags, show=True):
             # only plot normaldata for last trial to speed up things
             plot_model_normaldata = (c3d == c3ds[-1] and
                                      session == sessions[-1])
-            pl.plot_trial(model_tracecolor=linecolor,
+            pl.plot_trial(model_tracecolor=linecolors.pop(),
                           linestyles_context=True,
                           toeoff_markers=False, legend_maxlen=30,
                           maintitle='', superpose=True, show=False,
@@ -50,7 +52,6 @@ def do_comparison_plot(sessions, tags, show=True):
 
 
 def do_plot(tags=None, show=True, make_pdf=True):
-    """ One session consistency plot """
 
     c3ds = find_tagged(tags=tags)
 

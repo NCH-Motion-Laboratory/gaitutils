3348ccb5fcb5febdf2488f7ad27f01715a69dfea
Jussi Nurminen <jnu@iki.fi>
2018-05-23 10:26:54 +0300

    api fix for single trial autoproc

--- gaitutils/nexus_scripts/nexus_autoprocess_trial.py
+++ gaitutils/nexus_scripts/nexus_autoprocess_trial.py
@@ -8,16 +8,16 @@ Note: does not use stats for event detection -> less accurate
 """
 
 import logging
+import os.path as op
+
 from nexus_autoprocess_session import _do_autoproc
 from gaitutils import nexus, register_gui_exception_handler
 
 
 def autoproc_single():
-
-    vicon = nexus.viconnexus()
-    meta = nexus.get_metadata(vicon)
-    enfname = meta['sessionpath'] + meta['trialname'] + '.Trial.enf'
-    _do_autoproc([enfname])  # need to listify name
+    fn = nexus.get_trialname() + '.Trial.enf'
+    enfname = op.join(nexus.get_sessionpath(), fn)
+    _do_autoproc([enfname])
 
 
 if __name__ == '__main__':

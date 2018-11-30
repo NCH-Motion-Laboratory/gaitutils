979f59f4f18022a1d75a4a16533e8da8e9f064aa
Jussi Nurminen <jnu@iki.fi>
2018-11-30 10:34:30 +0200

    marker data read tests

--- tests/test_c3d.py
+++ tests/test_c3d.py
@@ -7,11 +7,12 @@ c3d unit tests.
 """
 
 
+import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 import logging
 
 from gaitutils.config import cfg
-from gaitutils import Trial, c3d, read_data
+from gaitutils import Trial, c3d, read_data, utils
 from gaitutils.utils import detect_forceplate_events
 from utils import run_tests_if_main, _trial_path, _c3d_path
 
@@ -60,10 +61,20 @@ def test_c3d_metadata():
 def test_c3d_marker_data():
     """Test marker data reads from c3d"""
     # lowerbody PiG marker set
-    c3dfile = _trial_path('girl6v', '2015_10_22_girl6v_IN02.c3d')
-    assert read_data.get_marker_data(c3dfile)
-    
-
+    c3dfile = _c3d_path('double_contact.c3d')
+    mkrset = utils._pig_markerset().keys()
+    mkrdata = read_data.get_marker_data(c3dfile, mkrset)
+    assert utils.is_plugingait_set(mkrdata)
+    assert len(mkrdata) == 5 * len(mkrset)  # P, P, V, A, gaps for all
+    # check array dimensions for all markers (gap data has different dims)
+    for mkr in mkrdata:
+        if '_gaps' not in mkr:
+            assert mkrdata[mkr].shape == (442, 3)
+    # LHEE gaps from 360 to 388
+    assert_equal(mkrdata['LHEE_gaps'], np.arange(360, 389))
+    lhee_data = np.load('lhee_data.npy')
+    # allow some deviation from saved reference data (was read using btk)
+    assert_allclose(mkrdata['LHEE'], lhee_data, rtol=1e-4)
 
 
 def test_c3d_fp_detection():

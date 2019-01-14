d0cddb51e13e6e18aa2e66b63133a791d82670c7
Jussi Nurminen <jnu@iki.fi>
2019-01-14 15:00:52 +0200

    proper error msg for no trial loaded

--- gaitutils/nexus_scripts/nexus_autoprocess_trial.py
+++ gaitutils/nexus_scripts/nexus_autoprocess_trial.py
@@ -14,11 +14,14 @@ import os.path as op
 
 from gaitutils.nexus_scripts.nexus_autoprocess_session import (_do_autoproc,
                                                                _delete_c3ds)
-from gaitutils import nexus, register_gui_exception_handler
+from gaitutils import nexus, register_gui_exception_handler, GaitDataError
 
 
 def autoproc_single():
-    fn = nexus.get_trialname() + '.Trial.enf'
+    fn = nexus.get_trialname()
+    if not fn:
+        raise GaitDataError('No trial open in Nexus')
+    fn += '.Trial.enf'
     enffiles = [op.join(nexus.get_sessionpath(), fn)]  # listify single enf
     _delete_c3ds(enffiles)
     _do_autoproc(enffiles)

e9011400af72bbf9ac6875b1304b3ac5342b0ec0
Jussi Nurminen <jnu@iki.fi>
2019-05-10 14:12:57 +0300

    try enum

--- clean_cfg.py
+++ clean_cfg.py
@@ -44,6 +44,12 @@ def not_whitespace(s):
     return bool(p.match(s))
 
 
+from enum import Enum
+class datatypes(Enum):
+    comments = 0
+    def_lines = 1
+    varlist = 2
+
 
 def clean_cfg(lines):
     
@@ -54,37 +60,41 @@ def clean_cfg(lines):
         if is_section_header(li):
             this_section = li
             di[this_section] = dict()
+            di[this_section][datatypes.varlist] = list()
+            di[this_section][datatypes.comments] = _comments
+            _comments = list()
         elif is_comment(li):
             # collect comments until we encounter next section header or variable
             _comments.append(li)
         elif is_var_def(li):
             var = li.split('=')[0]
+            di[this_section][datatypes.varlist].append(var)
             di[this_section][var] = dict()
-            di[this_section][var]['comments'] = _comments
-            di[this_section][var]['def_lines'] = list()
-            di[this_section][var]['def_lines'].append(li)
+            di[this_section][var][datatypes.comments] = _comments
+            di[this_section][var][datatypes.def_lines] = list()
+            di[this_section][var][datatypes.def_lines].append(li)
             _comments = list()
             # get indentation for list-type defs so it can be preserved
             if is_list_def(li):
                 idnt = max(li.find('['), li.find('{'))
         elif not_whitespace(li):  # continuation line
-            if var in di[this_section]:
+            if var in di[this_section][datatypes.varlist]:
                 if idnt is not None and idnt > 0:  # indent also def continuation lines
                     li = (idnt + 1) * ' ' + li.strip()
-                di[this_section][var]['def_lines'].append(li)
+                di[this_section][var][datatypes.def_lines].append(li)
             
     # compose output
     for sect in sorted(di.keys()):
         yield ''
         yield sect
         # whether section has var definitions spanning multiple lines
-        has_multiline_defs = any(len(di[sect][var]['def_lines']) > 1
-                                 for var in di[sect])
-        for var in sorted(di[sect].keys()):
+        has_multiline_defs = any(len(di[sect][var][datatypes.def_lines]) > 1
+                                 for var in di[sect][datatypes.varlist])
+        for var in sorted(di[sect][datatypes.varlist]):
             # print comments and definition for this var
-            if di[sect][var]['comments']:
-                yield '\n'.join(di[sect][var]['comments'])
-            def_this = di[sect][var]['def_lines']
+            if di[sect][var][datatypes.comments]:
+                yield '\n'.join(di[sect][var][datatypes.comments])
+            def_this = di[sect][var][datatypes.def_lines]
             yield '\n'.join(def_this)
             # output extra whitespace for sections that have multiline defs
             if has_multiline_defs:
@@ -97,4 +107,5 @@ with open(r"C:\Users\hus20664877\gaitutils\gaitutils\data\default.cfg", 'r') as 
 for li in clean_cfg(lines):
     print(li)
 
+lic = list(clean_cfg(lines))
 

4ce920a85a23460eabcc23cd1f94dda1709f2d3c
Jussi Nurminen <jnu@iki.fi>
2019-05-21 12:24:41 +0300

    setup.py fixes

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup, find_packages
 
 # entry points for console scripts
-c_entries = ['gaitmenu=gaitutils.gui._gaitmenu:main',
+console_entries = ['gaitmenu=gaitutils.gui._gaitmenu:main',
              'tardieu=gaitutils.gui._tardieu:main',
              'plotter_gui=gaitutils.gui._plotter_gui:main',
              'nexus_autoproc_session=gaitutils.autoprocess:autoproc_session',
@@ -14,20 +14,19 @@ c_entries = ['gaitmenu=gaitutils.gui._gaitmenu:main',
              'nexus_automark_trial=gaitutils.autoprocess:automark_trial',
              'nexus_plot_session_avg=gaitutils.viz.console:plot_nexus_session_average',
              'nexus_plot_trial=gaitutils.viz.console:plot_nexus_trial',
-             'nexus_plot_session=gaitutils.viz.console:plot_nexus_session',
-             'nexus_plot_session_emg=gaitutils.viz.console:plot_nexus_session_emg']
+             'nexus_plot_session=gaitutils.viz.console:plot_nexus_session']
 
 scripts = ['gaitutils/scripts/launch_menu.bat']
 
 setup(name='gaitutils',
-      version='0.11.32',
+      version='0.12',
       description='Utilities for processing and plotting gait data',
       author='Jussi Nurminen',
       author_email='jnu@iki.fi',
       license='GPLv3',
       url='https://github.com/jjnurminen/gaitutils',
       packages=find_packages(),
-      entry_points={'console_scripts': c_entries},
+      entry_points={'console_scripts': console_entries},
       scripts=scripts,
       include_package_data=True,
       )

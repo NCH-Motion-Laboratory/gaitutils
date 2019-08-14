cc8582c8e94e717086f1c526bf99659c2a51fde3
Jussi Nurminen <jnu@iki.fi>
2019-08-14 10:46:52 +0300

    add script header

--- gaitutils/gui/gaitmenu.py
+++ gaitutils/gui/gaitmenu.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Start the PyQt graphical interface to gaitutils
+
+@author: Jussi (jnu@iki.fi)
+"""
+
 from gaitutils.gui import _gaitmenu
 
 _gaitmenu.main()

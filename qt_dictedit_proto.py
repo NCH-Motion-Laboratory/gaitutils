804f0cfebb1c2d496d4a708544217199ec697c67
Nurminen Jussi <jnu@iki.fi>
2020-02-13 11:53:47 +0200

    wip

--- qt_dictedit_proto.py
+++ qt_dictedit_proto.py
@@ -3,10 +3,12 @@ Prototype for a Qt list/dict editor
 
 parameters: existing dict/list
 
--create scrollable area with as suitable amount of inputs for the dict/list
--use QFormLayout with labels and line edits (list) or two columns of line edits (dict)
--buttons: add / remove / cancel / ok
 
+TODO:
+    add dynamic headers (e.g. 'age range' and 'file' for age-specific normal data)
+    add option to use a file browser button instead of string editor (for dicts/lists of files)
+    lists are ordered, so maybe should have numbers on left column
+    use QListView instead of a grid layout?
 
 """
 
@@ -15,15 +17,29 @@ from PyQt5 import QtWidgets
 
 class QDictEditWindow(QtWidgets.QDialog):
 
-    def __init__(self, data, parent=None):
+    def __init__(self, data, key_hdr=None, val_hdr=None, parent=None):
 
+        self.data = data
+        self.is_list = isinstance(data, list)
         super(QDictEditWindow, self).__init__(parent=parent)
+        # the root layout
         box_layout = QtWidgets.QVBoxLayout()
+        # the scroll area
         scroll = QtWidgets.QScrollArea()
         scroll.setWidgetResizable(True)
+        # we cannot directly add the layout to the scroll area, but have to
+        # use a proxy widget that is a QWidget instance
         scroll_contents = QtWidgets.QWidget()
         scroll.setWidget(scroll_contents)
+        #
+        hdr_widget = QtWidgets.QWidget()
+        hdr_grid = QtWidgets.QGridLayout(hdr_widget)
+        hdr_grid.addWidget(QtWidgets.QLabel('foo'), 0, 0)
+        hdr_grid.addWidget(QtWidgets.QLabel('bar'), 0, 1)
+
+        box_layout.addWidget(hdr_widget)
         box_layout.addWidget(scroll)
+
         grid_layout = QtWidgets.QGridLayout(scroll_contents)
 
         std_buttons = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
@@ -38,32 +54,37 @@ class QDictEditWindow(QtWidgets.QDialog):
         buttonbox.accepted.connect(self.accept)
         buttonbox.rejected.connect(self.reject)
 
+        box_layout.add
         box_layout.addWidget(buttonbox)
 
-
-        if isinstance(data, list):
-            list_input = True
-            data = {k: i for k, i in enumerate(data)}
-        else:
-            list_input = False
-        for k, (key, val) in enumerate(data.items()):
-            if list_input:
-                grid_layout.addWidget(QtWidgets.QLabel(str(key)), k, 0)
-            else:
+        # generate the grid layout from the input data
+        if self.is_list:
+            for k, item in enumerate(self.data):
+                le_val = QtWidgets.QLineEdit()
+                grid_layout.addWidget(le_val, k, 0)
+                le_val.setText(str(item))
+        else:  # dict
+            for k, (key, val) in enumerate(self.data.items()):
                 le_key = QtWidgets.QLineEdit()
                 grid_layout.addWidget(le_key, k, 0)
                 le_key.setText(str(key))
-            le_val = QtWidgets.QLineEdit()
-            grid_layout.addWidget(le_val, k, 1)
-            le_val.setText(str(val))
+                le_val = QtWidgets.QLineEdit()
+                grid_layout.addWidget(le_val, k, 1)
+                le_val.setText(str(val))
         self.setLayout(box_layout)
 
+    def _add_item(self):
+        """Add a new dict/list item"""
+        pass
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     data = list('abcdef')
     data = {'a': 1, 'b': 2}
-    data = list(range(100))
+    #data = list(range(100))
+    #data = ['Toe standing', 'Unipedal right', 'Unipedal left']
+    data = {(7, 12): 'Z:\\PXD_files\\muscle_length_7_12.xlsx', (3, 6): 'Z:\\PXD_files\\muscle_length_3_6.xlsx', (13, 19): 'Z:\\PXD_files\\muscle_length_13_19.xlsx'}
+    #data = [['AnkleAnglesX', 'AnkleAnglesY', 'AnkleAnglesZ'], ['ForeFootAnglesX', 'ForeFootAnglesZ', 'ForeFootAnglesY']]
     window = QDictEditWindow(data)
     window.show()
     app.exec_()

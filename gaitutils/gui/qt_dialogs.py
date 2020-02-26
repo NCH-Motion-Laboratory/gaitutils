dbf5c424e7746158705771269e7aa33d37ff18c9
Nurminen Jussi <jnu@iki.fi>
2020-02-26 16:03:54 +0200

    wip: cool!

--- gaitutils/gui/qt_dialogs.py
+++ gaitutils/gui/qt_dialogs.py
@@ -15,6 +15,7 @@ import io
 from collections import defaultdict
 import logging
 
+from .qt_dictedit_proto import QCompoundEditorDict, QCompoundEditorList
 from .. import nexus, GaitDataError, cfg
 from ulstools import configdot
 from ..config import _handle_cfg_defaults, cfg_user_fn
@@ -81,68 +82,6 @@ def qt_dir_chooser():
     return file_dialog.selectedFiles() if file_dialog.exec_() else []
 
 
-class QCompoundEditor(QtWidgets.QDialog):
-    """WIP: copy from qt_dictedit_proto.py"""
-    def __init__(self, data, key_hdr=None, val_hdr=None, parent=None):
-
-        self.data = data
-        self.is_list = isinstance(data, list)
-        super(QCompoundEditor, self).__init__(parent=parent)
-        # the root layout
-        box_layout = QtWidgets.QVBoxLayout()
-        # the scroll area
-        scroll = QtWidgets.QScrollArea()
-        scroll.setWidgetResizable(True)
-        # we cannot directly add the layout to the scroll area, but have to
-        # use a proxy widget that is a QWidget instance
-        scroll_contents = QtWidgets.QWidget()
-        scroll.setWidget(scroll_contents)
-        #
-        hdr_widget = QtWidgets.QWidget()
-        hdr_grid = QtWidgets.QGridLayout(hdr_widget)
-        hdr_grid.addWidget(QtWidgets.QLabel('foo'), 0, 0)
-        hdr_grid.addWidget(QtWidgets.QLabel('bar'), 0, 1)
-
-        box_layout.addWidget(hdr_widget)
-        box_layout.addWidget(scroll)
-
-        grid_layout = QtWidgets.QGridLayout(scroll_contents)
-
-        std_buttons = QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
-        buttonbox = QtWidgets.QDialogButtonBox(std_buttons)
-
-        add_button = QtWidgets.QPushButton('Add')
-        del_button = QtWidgets.QPushButton('Delete')
-        buttonbox.addButton(add_button, QtWidgets.QDialogButtonBox.ActionRole)
-        buttonbox.addButton(del_button, QtWidgets.QDialogButtonBox.ActionRole)
-        #loadButton.clicked.connect(self.load_config_dialog)
-        #saveButton.clicked.connect(self.save_config_dialog)
-        buttonbox.accepted.connect(self.accept)
-        buttonbox.rejected.connect(self.reject)
-
-        #box_layout.add
-        box_layout.addWidget(buttonbox)
-
-        # generate the grid layout from the input data
-        if self.is_list:
-            for k, item in enumerate(self.data):
-                le_val = QtWidgets.QLineEdit()
-                grid_layout.addWidget(le_val, k, 0)
-                le_val.setText(str(item))
-        else:  # dict
-            for k, (key, val) in enumerate(self.data.items()):
-                le_key = QtWidgets.QLineEdit()
-                grid_layout.addWidget(le_key, k, 0)
-                le_key.setText(str(key))
-                le_val = QtWidgets.QLineEdit()
-                grid_layout.addWidget(le_val, k, 1)
-                le_val.setText(str(val))
-        self.setLayout(box_layout)
-
-    def _add_item(self):
-        """Add a new dict/list item"""
-        pass
-
 
 class OptionsDialog(QtWidgets.QDialog):
     """Dialog for changing gaitutils options"""
@@ -166,7 +105,8 @@ class OptionsDialog(QtWidgets.QDialog):
                 # launch the editor for complex types (list and dict)
                 input_widget = QtWidgets.QPushButton()
                 input_widget.setText('Edit...')
-                input_widget.clicked.connect(self._open_compound_editor)
+                # lambda needs to consume the extra value from connect(), hence x
+                input_widget.clicked.connect(lambda x, it=item: self._edit_with_compound_editor(it))
             else:
                 # for anything else, use a line edit with the literal value
                 input_widget = QtWidgets.QLineEdit()
@@ -212,10 +152,13 @@ class OptionsDialog(QtWidgets.QDialog):
         _main_layout.addWidget(self.buttonBox)
         self.setLayout(_main_layout)
 
-    def _open_compound_editor(self):
+    def _edit_with_compound_editor(self, item):
         """Opens a compound editor for selected config item"""
-        win = QCompoundEditor()
-        win.show()
+        val = item.value
+        editor = QCompoundEditorDict if isinstance(val, dict) else QCompoundEditorList
+        dlg = editor(val)
+        if dlg.exec_():
+            item.value = dlg.data
 
     def load_config_dialog(self):
         """Bring up load dialog and load selected file"""
--- gaitutils/gui/qt_dictedit_proto.py
+++ gaitutils/gui/qt_dictedit_proto.py
@@ -3,27 +3,20 @@ Prototype for a Qt list/dict editor
 
 parameters: existing dict/list
 
-
-TODO:
-    add dynamic headers (e.g. 'age range' and 'file' for age-specific normal data)
-    add option to use a file browser button instead of string editor (for dicts/lists of files)
-    lists are ordered, so maybe should have numbers on left column
-    use QListView instead of a grid layout?
-
 """
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialogButtonBox, QPushButton
 
 
-class QCompoundEditor(QtWidgets.QDialog):
+class QCompoundEditorDict(QtWidgets.QDialog):
+    """Compound editor for dicts"""
 
     def __init__(self, data, parent=None):
-        super(QCompoundEditor, self).__init__(parent=parent)
+        super(QCompoundEditorDict, self).__init__(parent=parent)
         root_layout = QtWidgets.QVBoxLayout()
         self.datable = QtWidgets.QTableWidget()
         self.data = data
-        self.list_mode = isinstance(data, list)
         # create the button box
         self.buttonbox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         insertbutton = QPushButton('Insert')
@@ -35,35 +28,23 @@ class QCompoundEditor(QtWidgets.QDialog):
         self.buttonbox.accepted.connect(self.accept)
         self.buttonbox.rejected.connect(self.reject)
         self.datable = QtWidgets.QTableWidget()
-        if self.list_mode:
-            self._init_for_list()
-        else:
-            self._init_for_dict()
+        self._init_table()
         root_layout.addWidget(self.datable)
         root_layout.addWidget(self.buttonbox)
         self.datable.resizeColumnsToContents()
         self.datable.setSelectionBehavior(QtWidgets.QTableView.SelectRows)
         self.setLayout(root_layout)
 
-    def _init_for_dict(self):
+    def _init_table(self):
         """Init table for dict data"""
         self.datable.setColumnCount(2)
         self.datable.setHorizontalHeaderLabels(['Key', 'Value'])
         #self.datable.setVerticalHeaderLabels()
-        for k, (key, val) in enumerate(data.items()):
+        for k, (key, val) in enumerate(self.data.items()):
             key = str(key)
             self.datable.insertRow(k)
             self.datable.setItem(k, 0, QtWidgets.QTableWidgetItem(key))
             self.datable.setItem(k, 1, QtWidgets.QTableWidgetItem(val))
-
-    def _init_for_list(self):
-        """Init table for list data"""
-        self.datable.setColumnCount(1)
-        self.datable.setHorizontalHeaderLabels(['Value'])
-        for k, val in enumerate(data):
-            val = str(val)
-            self.datable.insertRow(k)
-            self.datable.setItem(k, 0, QtWidgets.QTableWidgetItem(val))
 
     def _insert_item(self):
         """Insert a new dict/list item at current position"""
@@ -82,16 +63,28 @@ class QCompoundEditor(QtWidgets.QDialog):
 
     def accept(self):
         """Do some sanity checks, close dialog if ok"""
-        if self.list_mode:
-            self.data = self._collect_column(0)
-        else:
-            keys, vals = self._collect_column(0), self._collect_column(1)
-            self.data = {k: v for k, v in zip(keys, vals)}
+        keys, vals = self._collect_column(0), self._collect_column(1)
+        self.data = {k: v for k, v in zip(keys, vals)}
         self.done(QtWidgets.QDialog.Accepted)
 
 
-class QCompoundEditorList(QCompoundEditor):
-    
+class QCompoundEditorList(QCompoundEditorDict):
+    """Compound editor for lists"""
+
+    def _init_table(self):
+        """Init table for list data"""
+        self.datable.setColumnCount(1)
+        self.datable.setHorizontalHeaderLabels(['Value'])
+        for k, val in enumerate(self.data):
+            val = str(val)
+            self.datable.insertRow(k)
+            self.datable.setItem(k, 0, QtWidgets.QTableWidgetItem(val))
+
+    def accept(self):
+        """Do some sanity checks, close dialog if ok"""        
+        self.data = list(self._collect_column(0))
+        self.done(QtWidgets.QDialog.Accepted)
+
 
 
 
@@ -102,8 +95,8 @@ if __name__ == '__main__':
     #data = list(range(100))
     #data = ['Toe standing', 'Unipedal right', 'Unipedal left']
     data = {(7, 12): 'Z:\\PXD_files\\muscle_length_7_12.xlsx', (3, 6): 'Z:\\PXD_files\\muscle_length_3_6.xlsx', (13, 19): 'Z:\\PXD_files\\muscle_length_13_19.xlsx'}
-    #data = [['AnkleAnglesX', 'AnkleAnglesY', 'AnkleAnglesZ'], ['ForeFootAnglesX', 'ForeFootAnglesZ', 'ForeFootAnglesY']]
-    dlg = QCompoundEditor(data)
+    data = [['AnkleAnglesX', 'AnkleAnglesY', 'AnkleAnglesZ'], ['ForeFootAnglesX', 'ForeFootAnglesZ', 'ForeFootAnglesY']]
+    dlg = QCompoundEditorList(data)
     dlg.show()
     if dlg.exec_():
         print(dlg.data)

626152e0ccbecd6e98cf63729abd90360a169c94
Jussi Nurminen <jnu@iki.fi>
2020-03-04 15:48:53 +0200

    some tests for trial reads

--- tests/test_trial.py
+++ tests/test_trial.py
@@ -6,14 +6,14 @@ Test trial related functionality
 @author: jussi (jnu@iki.fi)
 """
 
-import pickle
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose, assert_equal
 import logging
 
-from gaitutils import read_data, utils
+from gaitutils import models
 from gaitutils.trial import Trial
-from gaitutils.utils import detect_forceplate_events
+from gaitutils.utils import _pig_markerset
 from utils import _trial_path, _c3d_path, _file_path, cfg
 
 
@@ -62,7 +62,74 @@ def test_get_cycles():
     tr = Trial(c3dfile)
     cycs = tr.get_cycles('forceplate')
     assert len(cycs) == 1
+    assert cycs[0].on_forceplate
     cycs = tr.get_cycles('all')
     assert len(cycs) == 4
     cycs = tr.get_cycles('all', max_cycles_per_context=1)
     assert len(cycs) == 2
+
+
+def test_trial_data_read():
+    """Test data read from normalized/unnormalized cycles"""
+    c3dfile = _trial_path('girl6v', '2015_10_22_girl6v_IN02.c3d')
+    tr = Trial(c3dfile)
+    with pytest.raises(ValueError):  # nonexistent cycle
+        tr.set_norm_cycle(10)
+    # read marker data
+    pig = _pig_markerset(fullbody=True, sacr=True)
+    for var in pig:
+        tr.set_norm_cycle(None)  # unnormalized
+        t, data = tr.get_marker_data(var)
+        assert t.shape == (794,)
+        assert data.shape == (794, 3)
+        tr.set_norm_cycle(0)  # unnormalized
+        t, data = tr.get_marker_data(var)
+        assert t.shape == (101,)
+        assert data.shape == (101, 3)
+        # some known values for this cycle
+        if var == 'SACR':
+            data_truth = np.array(
+                [
+                    [225.61265564, -1224.5526123, 693.34594727],
+                    [223.94009735, -1216.89548584, 692.26601624],
+                    [222.29524506, -1209.13790283, 691.30033325],
+                ]
+            )
+            assert_allclose(data[:3, :], data_truth)
+    # read model data
+    for var in list(models.pig_lowerbody.varnames) + list(
+        models.pig_upperbody.varnames
+    ):
+        tr.set_norm_cycle(None)  # unnormalized
+        t, data = tr.get_model_data(var)
+        assert t.shape == (794,)
+        assert data.shape == (794,)
+        tr.set_norm_cycle(0)  # unnormalized
+        t, data = tr.get_model_data(var)
+        assert t.shape == (101,)
+        assert data.shape == (101,)
+        # some known values for this cycle
+        if var == 'RHipAnglesX':
+            data_truth = np.array(
+                [
+                    5.12708759,
+                    4.58470859,
+                    4.0733108,
+                    3.60522768,
+                    3.1999483,
+                    2.87095979,
+                    2.62458159,
+                    2.47358855,
+                    2.44552759,
+                    2.53485509,
+                    2.75183632,
+                    3.12467247,
+                    3.65770569,
+                    4.35132931,
+                    5.21909947,
+                ]
+            )
+            assert_allclose(data[:15], data_truth)
+    # read EMG data
+    
+

28f0d838ac52b652e19afffe8fa37e75ebd39cd1
Jussi Nurminen <jnu@iki.fi>
2020-05-29 09:38:43 +0300

    bump ver

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ console_entries = [
 
 setup(
     name='gaitutils',
-    version='0.13.1',
+    version='0.13.2',
     description='Utilities for processing and plotting gait data',
     author='Jussi Nurminen',
     author_email='jnu@iki.fi',

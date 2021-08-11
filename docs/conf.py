f03f757476a7e77544ee0503132cd9b04d30350a
Jussi Nurminen <jnu@iki.fi>
2021-08-11 15:57:52 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,9 @@ author = 'Jussi Nurminen'
 extensions = [ 'sphinx.ext.napoleon', 'sphinx.ext.viewcode',
 ]
 
+napoleon_google_docstring = False
+napoleon_use_param = False
+napoleon_use_ivar = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

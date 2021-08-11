9af6859426c5dddd8637bcfd1cf882b8dbb18e95
Nurminen Jussi <jnu@iki.fi>
2021-08-11 15:27:08 +0300

    docs: wip

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../gaitutils'))
 
 # for autodoc
-autodoc_mock_imports = ['scipy']
+autodoc_mock_imports = ['scipy', 'configdot']
 
 # -- Project information -----------------------------------------------------
 
